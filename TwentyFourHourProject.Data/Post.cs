0268e4353f88789801fa0e9de0c0b2c2a1ccec51
bpcam <bpcampassi@gmail.com>
2021-02-04 23:36:56 -0500

    Added connection string and updated data object classes

--- TwentyFourHourProject.Data/Post.cs
+++ TwentyFourHourProject.Data/Post.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TwentyFourHourProject.Data
 {
-   public class Post
+    public class Post
     {
+        [Key]
+        public int PostId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Text { get; set; }
+        [Required]
+        public Guid Author { get; set; }
+
     }
 }
